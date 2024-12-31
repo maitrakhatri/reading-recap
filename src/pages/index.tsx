9058cdbc346285d9d1dcd5b642ece19c7c95d528
Maitra Khatri <maitrakhatri@gmail.com>
2024-12-31 14:42:43 +0530

    twitter link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,15 @@ export default function Home() {
       <div className="w-full rounded">
         <h1 className="text-3xl font-bold text-center">2024 Reading Recap</h1>
         <h2 className="font-semibold text-center my-2">
-          Made with ❤️ by Maitra Khatri
+          Made with ❤️ by{" "}
+          <a
+            href="https://x.com/maitrakhatri"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500"
+          >
+            Maitra Khatri
+          </a>
         </h2>
         <CirclePackingChart />
       </div>
